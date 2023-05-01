6c1913945a6a0d0032eda294f14c28f05b098c3e
smart-life-tech <christlightworld@gmail.com>
2023-05-01 22:09:21 +0100

    name	modified:   esp32MusicPlayer.ino

--- esp32MusicPlayer.ino
+++ esp32MusicPlayer.ino
@@ -1,13 +1,4 @@
 /*
-    Video: https://www.youtube.com/watch?v=oCMOYS71NIU
-    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
-    Ported to Arduino ESP32 by Evandro Copercini
-
-   Create a BLE server that, once we receive a connection, will send periodic notifications.
-   The service advertises itself as: 6E400001-B5A3-F393-E0A9-E50E24DCCA9E
-   Has a characteristic of: 6E400002-B5A3-F393-E0A9-E50E24DCCA9E - used for receiving data with "WRITE" 
-   Has a characteristic of: 6E400003-B5A3-F393-E0A9-E50E24DCCA9E - used to send data with  "NOTIFY"
-
    The design of creating the BLE server is:
    1. Create a BLE Server
    2. Create a BLE Service
@@ -69,7 +60,7 @@ void setup() {
   Serial.begin(115200);
 
   // Create the BLE Device
-  BLEDevice::init("UART Service");
+  BLEDevice::init("JM Music Player");
 
   // Create the BLE Server
   pServer = BLEDevice::createServer();
